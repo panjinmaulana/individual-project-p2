cbf835334733c41259d84957cee5679c4cd2ebcc
Panji Maulana <panjinmaulana@gmail.com>
2021-08-12 09:08:47 +0700

    helpers:jwt.js add jwt & .env-template

---
+++ Server/helpers/jwt.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken")
+
+const SECRET_KEY = process.env.SECRET_KEY
+
+function generateToken(payload) {
+   // synchronous Sign with default (HMAC SHA256)
+   const token = jwt.sign(payload, SECRET_KEY)
+
+   return token
+}
+
+function decodeToken(token) {
+   // verify a token symmetric - synchronous
+   const decoded = jwt.verify(token, SECRET_KEY)
+
+   return decoded
+}
+
+module.exports = {
+   generateToken,
+   decodeToken
+}
