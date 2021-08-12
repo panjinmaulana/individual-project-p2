e36c34f083ac07bf733ae8df17f8ed6fe4e85ad2
Panji Maulana <panjinmaulana@gmail.com>
2021-08-12 10:25:27 +0700

    fix require model:user in controllers:userControllers

--- Server/controllers/userControllers.js
+++ Server/controllers/userControllers.js
@@ -1,4 +1,4 @@
-const User = require("../models")
+const { User } = require("../models")
 const { comparePassword } = require("../helpers/bcrypt")
 const { generateToken } = require("../helpers/jwt")
 
