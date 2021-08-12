7d01ccc292295a8cc4b2dfad483931e501ab81f4
Panji Maulana <panjinmaulana@gmail.com>
2021-08-12 13:17:29 +0700

    controllers:movieControllers get movie popular from tmdb, add tmdb_token at .env & add package axios

--- Server/controllers/movieControllers.js
+++ Server/controllers/movieControllers.js
@@ -1,5 +1,19 @@
+// axios.<method> will now provide autocomplete and parameter typings
+const axios = require("axios").default
+const TMDB_URL = "https://api.themoviedb.org/3/movie"
+const TMDB_TOKEN = process.env.TMDB_TOKEN
 class MovieController {
-   static popular(req, res, next) {}
+   static popular(req, res, next) {
+      axios({
+         method: "GET",
+         url: `${TMDB_URL}/popular`,
+         headers: {
+            Authorization: `Bearer ${TMDB_TOKEN}`
+         }
+      })
+         .then(({ data }) => res.status(200).json({ success: true, data }))
+         .catch((err) => next(err))
+   }
 }
 
 module.exports = MovieController
