e0b26c1e15acda08f860d1a1a95ec06e8243ba24
Panji Maulana <panjinmaulana@gmail.com>
2021-08-12 08:07:54 +0700

    add path routes

--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,9 @@ require("dotenv").config()
 
 const express = require("express")
 const cors = require("cors")
+const routes = require("./routes")
 const app = express()
+
 const port = 3000
 
 app.use(cors())
